bddc137d2a33ac8b1f04099f94714567302d1ee1
robin-whg <mail@robin-whg.com>
2022-01-28 16:33:01 +0100

    feat: darkmode

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
 import { useHead } from "@vueuse/head"
+import { useDark } from "@vueuse/core"
 
 useHead({
   title: "Vue Three",
   meta: [
     {
       name: "description",
-      content: "Drag and Drop 3D Viewer made with ThreeJS and Vue 3 ",
+      content: "Drag and Drop 3D Viewer made with ThreeJS and Vue 3",
     },
   ],
 })
+
+useDark()
 </script>
 
 <template>
