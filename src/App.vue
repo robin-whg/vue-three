ad2caa3b00fb14a53828cdb68456bdbd8c65af48
robin-whg <mail@robin-whg.com>
2022-01-28 15:04:47 +0100

    feat: setup styling

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import { useHead } from "@vueuse/head"
 
 useHead({
-  title: "Vute Template",
-  meta: [{ name: "description", content: "Template for Vue Projects" }],
+  title: "Vue Three",
+  meta: [
+    {
+      name: "description",
+      content: "Drag and Drop 3D Viewer made with ThreeJS and Vue 3 ",
+    },
+  ],
 })
 </script>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#10B981",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 }
